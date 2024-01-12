786f9834518c06cb401fbd6036d25b6a9a983d3a
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2024-01-11 21:11:44 -0300

    inicio do projeto, acessando o token g2a(expira em 1 hora)

--- index.js
+++ index.js
@@ -1,55 +1,39 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const axios = require('axios');
 require('dotenv').config();
+
+const url = process.env.URL;
+const client_id = process.env.G2A_ClientID;
+const client_secret = process.env.G2A_ClientSecret;
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-      res.send('Server online');
+      res.send('Working');
 })
-  
+
+app.get('/token', async (req, res) => {
+      try {
+            // Faça a consulta à outra API usando Axios ou a biblioteca de sua escolha
+            const grant_type = "client_credentials";
+            
+            const response = await axios.post(`${url}/oauth/token?grant_type=${grant_type}&client_id=${client_id}&client_secret=${client_secret}`, { // url + endpoint + token G2A
+                  // headers: {
+                  //       'Authorization': `Bearer ${token}`
+                  // },
+            });
+            // Retorne os dados da consulta
+            res.json(response.data); // nossa resposta
+      } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao consultar a API externa.' });
+      }
+})
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
       console.log(`Listening to port ${port}.`);
 })
-
-
-
-
-
-
-
-
-
-
-  // (async () => {
-  //   // Launch the browser and open a new blank page
-  //   const browser = await puppeteer.launch({
-  //     headless: false
-  //   });
-  
-  //   const page = await browser.newPage();
-  
-  //   // Navigate the page to a URL
-  //   await page.goto('https://www.g2a.com/pt/');
-  
-  //   // Set screen size
-  //   await page.setViewport({ width: 1920, height: 1080 });
-  
-  
-  //   //   // Type into search box
-  //   //   await page.type('.topbar-button-5d5f2afa-dc93-44e0-bbed-71501fdd4f94', 'automate beyond recorder');
-  
-  //   //   // Wait and click on login button
-  //   const loginSelector = '.indexes__StyledButton-kft35s-1.cIPKCC';
-  //   await page.waitForSelector(loginSelector);
-  //   await page.click(loginSelector);
-  
-  //   const signInSelector = '.indexes__Button-kft35s-8.gocpYy';
-  //   await page.waitForSelector(signInSelector);
-  //   await page.click(signInSelector);
-    
-  
-  // })();
